3b12ab458bf87cc1d2ad057b4354066cedb4480f
Austin Acosta <aa6614293@gmail.com>
2023-02-15 09:53:00 -0500

    added the project
    + creating the actual project
    + added a debug script to make sure everything is working

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Oh gee, uh, just putting this script here to test stuff out, uhm, making sure that the repo can update properly.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
